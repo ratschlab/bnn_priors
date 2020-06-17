08df7749e6621f5a4e88bf6218f6c88b05aae4a4
Vincent Fortuin <vincent.fortuin@gmail.com>
2020-06-17 15:57:29 +0000

    Fix stupid mistake with the noise injection

--- bnn_priors/inference.py
+++ bnn_priors/inference.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 import torch
 from pyro.distributions import Normal
 
@@ -14,25 +15,36 @@ class SGLD:
         self.optimizer = torch.optim.SGD(params=self.model.parameters(),
                                          lr=learning_rate)
 
-    def run(self, x, y):
-        for _ in range(self.warmup_steps):
+        
+    def run(self, x, y, progressbar=False):
+        if progressbar:
+            warmup_iter = tqdm(range(self.warmup_steps), position=0, leave=True)
+            sampling_iter = tqdm(range(self.num_samples), position=0, leave=True)
+        else:
+            warmup_iter = range(self.warmup_steps)
+            sampling_iter = range(self.num_samples)
+        for _ in warmup_iter:
             self.step(x, y)
-        for i in range(self.num_samples):
+        for i in sampling_iter:
             self.step(x, y)
             for param, value in self.model.state_dict().items():
                 self._samples[param][i] = value
 
+                
     def step(self, x, y):
         self.optimizer.zero_grad()
+        # TODO: this only works when the full data is used, otherwise the log_likelihood should be rescaled according to the batch size
         loss = self.model.potential(x, y)
         loss.backward()
-        for param in self.model.parameters():
-            param.grad.add_(
-                torch.randn(param.grad.size(), dtype=param.dtype,
-                            device=param.device)
-                .mul_(np.sqrt(2*self.learning_rate)))
         self.optimizer.step()
+        with torch.no_grad():
+            for param in self.model.parameters():
+                param.add_(
+                    torch.randn(param.size(), dtype=param.dtype,
+                                device=param.device)
+                    .mul_(np.sqrt(2*self.learning_rate)))
         return loss.item()
 
+    
     def get_samples(self):
         return self._samples
