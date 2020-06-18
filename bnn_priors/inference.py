b2acbfa773830a919ef5efc0ec34df0e374546a5
Vincent Fortuin <vincent.fortuin@gmail.com>
2020-06-18 12:51:50 +0000

    Add skipping steps to SGLD inference to reduce sample correlation

--- bnn_priors/inference.py
+++ bnn_priors/inference.py
@@ -5,11 +5,13 @@ from pyro.distributions import Normal
 
 
 class SGLD:
-    def __init__(self, model, num_samples, warmup_steps, learning_rate=5e-4, decay_rate=1.0):
+    def __init__(self, model, num_samples, warmup_steps,
+                 learning_rate=5e-4, decay_rate=1.0, skip=1):
         self.model = model
         self.num_samples = num_samples
         self.warmup_steps = warmup_steps
         self.learning_rate = learning_rate
+        self.skip = skip
         self._samples = {name : torch.zeros(torch.Size([num_samples])+param.shape)
                          for name, param in self.model.params_with_prior()}
         self.optimizer = torch.optim.SGD(params=self.model.parameters(),
@@ -21,16 +23,17 @@ class SGLD:
     def run(self, x, y, progressbar=False):
         if progressbar:
             warmup_iter = tqdm(range(self.warmup_steps), position=0, leave=True)
-            sampling_iter = tqdm(range(self.num_samples), position=0, leave=True)
+            sampling_iter = tqdm(range(self.num_samples * self.skip), position=0, leave=True)
         else:
             warmup_iter = range(self.warmup_steps)
-            sampling_iter = range(self.num_samples)
+            sampling_iter = range(self.num_samples * self.skip)
         for _ in warmup_iter:
             self.step(x, y, lr_decay=True)
         for i in sampling_iter:
             self.step(x, y, lr_decay=False)
-            for param, value in self.model.state_dict().items():
-                self._samples[param][i] = value
+            if i % self.skip == 0:
+                for param, value in self.model.state_dict().items():
+                    self._samples[param][i//self.skip] = value
 
                 
     def step(self, x, y, lr_decay=False):
