859fb501e238ca4bbb750efa3d51e9923b4a8951
Vincent Fortuin <vincent.fortuin@gmail.com>
2020-07-10 15:01:01 +0200

    Fix parameters in SGLD test

--- testing/test_sgld.py
+++ testing/test_sgld.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 import torch
 from bnn_priors.models import DenseNet
 from bnn_priors.inference import SGLDRunner
@@ -16,12 +17,12 @@ class SGLDTest(unittest.TestCase):
         x_test = torch.from_numpy(data['x_test']).unsqueeze(1).to(x_train)
 
         N_steps = 10
-        skip = 50
-        warmup = 500
-        cycles =  5
+        skip = 20
+        warmup = 50
+        cycles =  1
         temperature = 1.0
         momentum = 0.9
-        precond_update = 50
+        precond_update = 10
         lr = 5e-4
 
         model = DenseNet(x_train, y_train, 128, noise_std=0.5)
@@ -33,9 +34,13 @@ class SGLDTest(unittest.TestCase):
         sgld = SGLDRunner(model=model, num_samples=N_steps, warmup_steps=warmup, learning_rate=lr,
                           skip=skip, sampling_decay=True, cycles=cycles, temperature=temperature,
                           momentum=momentum, precond_update=precond_update)
-        sgld.run(x=x_train, y=y_train, progressbar=True)
+        sgld.run(x=x_train, y=y_train, progressbar=False)
 
         assert sgld.metrics["loss"][0] > sgld.metrics["loss"][-1]
         assert sgld.metrics["lr"][0] > sgld.metrics["lr"][1]
         assert (sgld.metrics["preconditioner/latent_fn.0.weight_prior"][0]
                 != sgld.metrics["preconditioner/latent_fn.0.weight_prior"][-1])
+
+
+if __name__ == '__main__':
+    unittest.main()
