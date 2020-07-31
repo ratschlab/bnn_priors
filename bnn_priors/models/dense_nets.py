Fix prior reparameterization and add new flexible scaling function to the DenseNet models

bnn_priors/models/dense_nets.py
@@ -11,24 +11,31 @@ def LinearNealNormal(in_dim, out_dim, std_w, std_b):
 
 
 def LinearPrior(in_dim, out_dim, prior_w=prior.Normal, loc_w=0., std_w=1.,
-                     prior_b=prior.Normal, loc_b=0., std_b=1.):
-    return Linear(prior_w((out_dim, in_dim), loc_w, std_w/in_dim**.5),
+                     prior_b=prior.Normal, loc_b=0., std_b=1., scaling_fn=None):
+    if scaling_fn is None:
+        def scaling_fn(std, dim):
+            return std/dim**0.5
+    return Linear(prior_w((out_dim, in_dim), loc_w, scaling_fn(std_w, in_dim)),
                   prior_b((out_dim,), 0., std_b))
 
 
 def DenseNet(in_features, out_features, width, noise_std=1.,
              prior_w=prior.Normal, loc_w=0., std_w=2**.5,
-             prior_b=prior.Normal, loc_b=0., std_b=1.):
+             prior_b=prior.Normal, loc_b=0., std_b=1.,
+            scaling_fn=None):
     return RegressionModel(
         nn.Sequential(
             LinearPrior(in_features, width, prior_w=prior_w, loc_w=loc_w,
-                       std_w=std_w, prior_b=prior_b, loc_b=loc_b, std_b=std_b),
+                       std_w=std_w, prior_b=prior_b, loc_b=loc_b, std_b=std_b,
+                       scaling_fn=scaling_fn),
             nn.ReLU(),
             LinearPrior(width, width, prior_w=prior_w, loc_w=loc_w,
-                       std_w=std_w, prior_b=prior_b, loc_b=loc_b, std_b=std_b),
+                       std_w=std_w, prior_b=prior_b, loc_b=loc_b, std_b=std_b,
+                       scaling_fn=scaling_fn),
             nn.ReLU(),
             LinearPrior(width, out_features, prior_w=prior_w, loc_w=loc_w,
-                       std_w=std_w, prior_b=prior_b, loc_b=loc_b, std_b=std_b)
+                       std_w=std_w, prior_b=prior_b, loc_b=loc_b, std_b=std_b,
+                       scaling_fn=scaling_fn)
         ), noise_std)
 
 

bnn_priors/prior/loc_scale.py
@@ -17,21 +17,20 @@ class LocScale(Prior):
        scale (float, torch.Tensor, prior.Prior): scale
     """
     def __init__(self, shape, loc, scale):
-        super().__init__(shape, 0., 1.)
+        super().__init__(shape, loc, scale)
         self.loc = loc
         self.scale = scale
 
     def forward(self):
-        return self.loc + self.scale * self.p
+        return self.p
 
 
 class Normal(LocScale):
     _dist = td.Normal
 
+    
 class LogNormal(LocScale):
-    _dist = td.Normal
-    def forward(self):
-        return (self.loc + self.scale * self.p).exp()
+    _dist = td.LogNormal
 
 
 class Laplace(LocScale):
@@ -45,7 +44,7 @@ class Cauchy(LocScale):
 class StudentT(LocScale):
     _dist = td.StudentT
     def __init__(self, shape, loc, scale, df=1):
-        Prior.__init__(self, shape, df=df, loc=0., scale=1.)
+        Prior.__init__(self, shape, df=df, loc=loc, scale=scale)
         self.loc = loc
         self.scale = scale
 
@@ -61,12 +60,6 @@ class Uniform(LocScale):
     """
     _dist = td.Normal
     def __init__(self, shape, low, high):
-        super().__init__(shape, 0., 1.)
+        super().__init__(shape, low, high)
         self.low = low
         self.high = high
-
-    def forward(self):
-        dist = self.dist()
-        uniform = dist.cdf(self.p)
-        # uniform = torch.nn.functional.sigmoid(self.p)
-        return self.low + (self.high-self.low) * uniform