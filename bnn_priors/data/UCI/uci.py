02fbc38c61041a1bd389f13ab8e6188ff904607a
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2020-08-17 12:12:15 +0100

    Dataset old comment out

--- bnn_priors/data/UCI/uci.py
+++ bnn_priors/data/UCI/uci.py
@@ -8,23 +8,6 @@ from bnn_priors.data import Dataset
 __all__ = ('UCI',)
 
 
-# class Dataset:
-#     """
-#     Represents the full dataset.  We will have two copies: one normalised, one unnormalized.
-#     """
-#     def __init__(self, X, y, index_train, index_test):
-#         self.X = X
-#         self.y = y
-
-#         self.train_X = self.X[index_train]
-#         self.train_y = self.y[index_train]
-#         self.test_X  = self.X[index_test]
-#         self.test_y  = self.y[index_test]
-
-#         self.train = TensorDataset(self.train_X, self.train_y)
-#         self.test  = TensorDataset(self.test_X,  self.test_y)
-
-        
 class UCI:
     """
     The usage is:
