c9ecf96bda5d7c662f278c07ba5d8c1bd9ee3a10
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2020-08-24 13:05:07 +0100

    Deduplicate CIFAR10-C and CIFAR10 loading

--- bnn_priors/data/CIFAR/cifar.py
+++ bnn_priors/data/CIFAR/cifar.py
@@ -27,15 +27,18 @@ class CIFAR10:
         data_train = torchvision.datasets.CIFAR10(dataset_dir, download=True, train=True)
         data_test = torchvision.datasets.CIFAR10(dataset_dir, download=True, train=False)
 
+        self._save_datasets(data_train.data, data_test.data, data_train.targets, data_test.targets)
+
+    def _save_datasets(self, inputs_train, inputs_test, labels_train, labels_test):
         # get data into right shape and type
-        X_unnorm = t.from_numpy(np.concatenate([data_train.data, data_test.data]).astype(dtype)).permute(0,3,1,2)
-        y = t.from_numpy(np.concatenate([data_train.targets, data_test.targets]).astype('int'))
+        X_unnorm = t.from_numpy(np.concatenate([inputs_train, inputs_test]).astype(dtype)).permute(0,3,1,2)
+        y = t.from_numpy(np.concatenate([labels_train, labels_test]).astype('int'))
         # alternative version to yield one-hot vectors
         # y = t.from_numpy(np.eye(10)[np.concatenate([data_train.targets, data_test.targets])].astype(dtype))
         
         # train / test split
-        index_train = np.arange(len(data_train))
-        index_test = np.arange(len(data_train), len(data_train) + len(data_test))
+        index_train = np.arange(len(inputs_train))
+        index_test = np.arange(len(inputs_train), len(inputs_train) + len(inputs_test))
         
         # create unnormalized data set
         self.unnorm = Dataset(X_unnorm, y, index_train, index_test, device)
@@ -100,26 +103,4 @@ class CIFAR10_C:
         data_test = np.load(os.path.join(dataset_dir_corrupted, f"{corruption}.npy"))
         labels = np.load(os.path.join(dataset_dir_corrupted, f"labels.npy"))
 
-        # get data into right shape and type
-        X_unnorm = t.from_numpy(np.concatenate([data_train.data, data_test]).astype(dtype)).permute(0,3,1,2)
-        y = t.from_numpy(np.concatenate([data_train.targets, labels]).astype('int'))
-        
-        # train / test split
-        index_train = np.arange(len(data_train))
-        index_test = np.arange(len(data_train), len(data_train) + len(data_test))
-        
-        # create unnormalized data set
-        self.unnorm = Dataset(X_unnorm, y, index_train, index_test, device)
-        
-        # compute normalization constants based on training set
-        self.X_std = t.std(self.unnorm.train_X, (0, 2, 3), keepdims=True)
-        self.X_mean = t.mean(self.unnorm.train_X, (0, 2, 3), keepdims=True)
-        
-        # create normalized data set
-        X_norm = (self.unnorm.X - self.X_mean)/self.X_std
-        self.norm = Dataset(X_norm, y, index_train, index_test, device)
-
-        # save some data shapes
-        self.num_train_set = self.unnorm.X.shape[0]
-        self.in_shape   = self.unnorm.X.shape[1:]
-        self.out_shape  = self.unnorm.y.shape[1:]
+        self._save_datasets(data_train.data, data_test, data_train.targets, labels)
