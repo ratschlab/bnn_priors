9597d1395b90e7ec52445aa4bf977781ff03d31b
Vincent Fortuin <vincent.fortuin@gmail.com>
2020-09-04 16:58:09 +0200

    Modify experiment scripts

--- experiments/experiment_mnist.sh
+++ experiments/experiment_mnist.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-priors=( gaussian uniform laplace student-t cauchy improper gaussian_gamma gaussian_uniform horseshoe )
-scales=( 0.14 1.41 14.1 )
+logdir='../results/200904_mnist'
+priors=( gaussian uniform laplace student-t cauchy improper gaussian_gamma gaussian_uniform horseshoe laplace_gamma laplace_normal student-t_gamma student-t_normal mixture )
+scales=( 0.7 1.41 2.41 )
 temps=( 0.0 0.1 1.0 )
 
 for prior in "${priors[@]}"
@@ -11,7 +12,7 @@ do
         for temp in "${temps[@]}"
         do
 
-           bsub -n 2 -W 8:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=8000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=mnist inference=SGLD model=classificationdensenet width=100 warmup=30 burnin=10 skip=1 n_samples=100 lr=0.01 weight_scale=$scale cycles=20 batch_size=64 temperature=$temp save_samples=True"
+           bsub -n 2 -W 8:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=8000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=mnist inference=SGLD model=classificationdensenet width=100 warmup=30 burnin=10 skip=1 n_samples=100 lr=0.01 weight_scale=$scale cycles=20 batch_size=64 temperature=$temp save_samples=True log_dir=$logdir"
         done
     done
 done
--- experiments/experiment_uci.sh
+++ experiments/experiment_uci.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+logdir='../results/200904_uci'
 priors=( gaussian uniform laplace student-t cauchy improper gaussian_gamma gaussian_uniform horseshoe laplace_gamma laplace_normal student-t_gamma student-t_normal mixture )
 datasets=( boston wine energy naval concrete kin8nm power yacht protein )
 inference=( SGLD )  # add HMC if needed
@@ -16,7 +17,7 @@ do
             do
                 for temp in "${temps[@]}"
                 do
-                    bsub -n 2 -W 4:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=4000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=UCI_$dataset inference=$inf warmup=5000 burnin=1000 weight_scale=$scale cycles=20 n_samples=100 skip=100 temperature=$temp"
+                    bsub -n 2 -W 4:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=4000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=UCI_$dataset inference=$inf warmup=5000 burnin=1000 weight_scale=$scale cycles=20 n_samples=100 skip=100 temperature=$temp log_dir=$logdir"
                 done
             done
         done
