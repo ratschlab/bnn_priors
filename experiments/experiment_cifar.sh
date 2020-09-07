1c3143e4b68d7b0bb2c3aeac44f599291e2f16cf
Vincent Fortuin <vincent.fortuin@gmail.com>
2020-09-07 13:32:07 +0200

    Evaluate experiments

--- experiments/experiment_cifar.sh
+++ experiments/experiment_cifar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 logdir='../results/200905_cifar'
-priors=( gaussian uniform laplace student-t cauchy improper gaussian_gamma gaussian_uniform horseshoe laplace_gamma laplace_normal student-t_gamma student-t_normal mixture )
+priors=( gaussian uniform laplace student-t cauchy improper gaussian_gamma gaussian_uniform horseshoe laplace_gamma laplace_uniform student-t_gamma student-t_uniform mixture )
 scales=( 0.7 1.41 2.41 )
 temps=( 0.0 0.1 1.0 )
 
@@ -12,7 +12,7 @@ do
         for temp in "${temps[@]}"
         do
 
-           bsub -n 2 -W 24:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=8000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=cifar10 inference=SGLD model=resnet18 warmup=30 burnin=10 skip=1 n_samples=100 lr=0.01 weight_scale=$scale cycles=20 batch_size=128 temperature=$temp save_samples=True log_dir=$logdir"
+           bsub -n 2 -W 24:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=16000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=cifar10 inference=SGLD model=resnet18 warmup=30 burnin=10 skip=1 n_samples=100 lr=0.01 weight_scale=$scale cycles=20 batch_size=128 temperature=$temp save_samples=True log_dir=$logdir"
         done
     done
 done
--- experiments/experiment_uci.sh
+++ experiments/experiment_uci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 logdir='../results/200904_uci'
-priors=( gaussian uniform laplace student-t cauchy improper gaussian_gamma gaussian_uniform horseshoe laplace_gamma laplace_normal student-t_gamma student-t_normal mixture )
+priors=( gaussian uniform laplace student-t cauchy improper gaussian_gamma gaussian_uniform horseshoe laplace_gamma laplace_uniform student-t_gamma student-t_uniform mixture )
 datasets=( boston wine energy naval concrete kin8nm power yacht protein )
 inference=( SGLD )  # add HMC if needed
 scales=( 0.7 1.41 2.41 )
@@ -17,7 +17,7 @@ do
             do
                 for temp in "${temps[@]}"
                 do
-                    bsub -n 2 -W 4:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=4000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=UCI_$dataset inference=$inf warmup=5000 burnin=1000 weight_scale=$scale cycles=20 n_samples=100 skip=100 temperature=$temp log_dir=$logdir"
+                    bsub -n 2 -W 24:00 -J "bnn" -sp 40 -g /vincent/experiments -G ms_raets -R "rusage[mem=4000,ngpus_excl_p=1]" "source activate bnn; python train_bnn.py with weight_prior=$prior data=UCI_$dataset inference=$inf warmup=5000 burnin=1000 weight_scale=$scale cycles=20 n_samples=100 skip=100 temperature=$temp log_dir=$logdir"
                 done
             done
         done
