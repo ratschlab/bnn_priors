185d34fecc3bb876f8f26070d7438608d3b037c8
Vincent Fortuin <vincent.fortuin@gmail.com>
2020-09-09 14:52:55 +0200

    Update the parsing of components for the Mixture prior

--- bnn_priors/prior/mixture.py
+++ bnn_priors/prior/mixture.py
@@ -32,10 +32,8 @@ def get_prior(prior_name):
 
 
 class Mixture(LocScale):
-    def __init__(self, shape, loc, scale, components=None):
-        if components is None:
-            components = ["gaussian", "laplace", "student-t", "cauchy"]
-        assert len(components) > 0, "Too few mixture components"
+    def __init__(self, shape, loc, scale, components="g_l_s_c"):
+        components=self.get_components(components)
         super().__init__(shape, loc, scale)
         self.mixture_weights = torch.nn.Parameter(torch.zeros(len(components)))
         self.components = [get_prior(comp)(shape, loc, scale)
@@ -45,11 +43,40 @@ class Mixture(LocScale):
         for i, comp in enumerate(self.components):
             self.add_module(f"component_{i}", comp)
         
+
     _dist = td.Normal
     
+
     def log_prob(self):
+        """
+        Return weighted sum of the components' log probs.
+        """
         weights = torch.nn.functional.softmax(self.mixture_weights, dim=0)
         probs = [weight*comp.log_prob() for weight, comp in zip(weights, self.components)]
         return sum(probs)
     
-    
+
+    @staticmethod
+    def get_components(comp_string):
+        """
+        Parse the component string into the components.
+        """
+        comp_dict = {"g": "gaussian",
+                    "ln": "lognormal",
+                    "l": "laplace",
+                    "c": "cauchy",
+                    "s": "student-t",
+                    "u": "uniform",
+                    "i": "improper",
+                    "gg": "gaussian_gamma",
+                    "gu": "gaussian_uniform",
+                    "h": "horseshoe",
+                    "lg": "laplace_gamma",
+                    "lu": "laplace_uniform",
+                    "sg": "student-t_gamma",
+                    "su": "student-t_uniform"}
+        abrvs = comp_string.split("_")
+        assert all([abrv in comp_dict for abrv
+                    in abrvs]), "Unknown mixture components"
+        components = [comp_dict[abrv] for abrv in abrvs]
+        return components
