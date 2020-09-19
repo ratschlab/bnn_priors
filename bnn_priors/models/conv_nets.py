c47b5a19a249daa5a99c53cc319dc3a32f2108b2
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2020-09-19 21:54:12 +0100

    Fixed bug

--- bnn_priors/models/conv_nets.py
+++ bnn_priors/models/conv_nets.py
@@ -11,7 +11,7 @@ from .base import RegressionModel, ClassificationModel
 from .dense_nets import LinearPrior
 from .. import prior
 
-__all__ = ('Conv2dPrior', 'PreActResNet18', 'PreActResNet34', 'ClassificationConvNet')
+__all__ = ('Conv2dPrior', 'PreActResNet18', 'PreActResNet34', 'ClassificationConvNet', 'ThinPreActResNet18')
 
 def Conv2dPrior(in_channels, out_channels, kernel_size=3, stride=1,
             padding=0, dilation=1, groups=1, padding_mode='zeros',
@@ -131,15 +131,16 @@ class PreActResNet(nn.Module):
         self.weight_prior_params = weight_prior_params
         self.bias_prior_params = bias_prior_params
 
-        self.conv1 = Conv2dPrior(3, 64, kernel_size=3, stride=1, padding=1, prior_b=None,
+        # `self.in_planes` gets modified, so we use `in_planes`.
+        self.conv1 = Conv2dPrior(3, in_planes, kernel_size=3, stride=1, padding=1, prior_b=None,
                            prior_w=self.prior_w, loc_w=self.loc_w, std_w=self.std_w,
                            scaling_fn=self.scaling_fn, weight_prior_params=self.weight_prior_params,
                             bias_prior_params=self.bias_prior_params)
-        self.layer1 = self._make_layer(block,     self.in_planes, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 2 * self.in_planes, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 4 * self.in_planes, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 8 * self.in_planes, num_blocks[3], stride=2)
-        self.linear = LinearPrior(8 * self.in_planes * block.expansion, num_classes,
+        self.layer1 = self._make_layer(block, in_planes, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 2 * in_planes, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 4 * in_planes, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 8 * in_planes, num_blocks[3], stride=2)
+        self.linear = LinearPrior(8 * in_planes, num_classes,
                             prior_w=self.prior_w, loc_w=self.loc_w, std_w=self.std_w,
                             prior_b=self.prior_b, loc_b=self.loc_b, std_b=self.std_b,
                             scaling_fn=self.scaling_fn, weight_prior_params=self.weight_prior_params,
