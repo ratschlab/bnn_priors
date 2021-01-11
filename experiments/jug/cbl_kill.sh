692c08b9fe581f4960137c49d91fe3db581fe93a
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2021-01-11 17:30:10 +0100

    Run SGD automatically for the UCI

--- experiments/jug/cbl_kill.sh
+++ experiments/jug/cbl_kill.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 for host in cartwright laplace poisson vapnik gosset julia fields \
     ramanujan banach riemann euler robbins vartak curie sagarmatha ariadne \
     grothendieck babbage neumann gan bernoulli; do
-    ssh "$host" 'ps aux | grep ag919'
+    ssh "$host" 'ps aux | grep ag919' || continue;
     echo "For host $host ([0,1,2,...])? Should I kill (write \`kill\`)?"
     read command
     if [ "$command" = "kill" ]; then
